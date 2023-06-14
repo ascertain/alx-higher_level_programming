4d6d9de29007e5556492b313477042d3d75faf5c
ascertain <kas.naya@gmail.com>
2023-06-14 09:53:45 -0700

    py more DS

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,29 +1,33 @@
-#!/usr/bin/python3
-def roman_to_int(roman_string):
-    """Converts a roman numeral to an integer."""
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
-
-    roman_dict = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-    }
-    num = 0
-
-    for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
-
-        if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
-
-        else:
-            num += roman_dict[roman_string[i]]
-    return (num)
+'''
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+'''
+class Solution:
+    def romanToInt(self, s: 'str') -> 'int':
+        value_roman = {"M":1000, "CM":900, "D":500, "CD": 400,
+                       "C":100,"XC":90, "L":50, "XL":40,
+                       "X":10, "IX":9, "V":5, "IV":4, "I":1}
+        num = 0
+        specials_list = ["CM","CD","XC","XL","IX","IV"]
+        for i in specials_list:
+            if i in s:
+                num = num + value_roman[i]
+                s=s.replace(i,"")
+        for i in s:
+            num = num + value_roman[i]
+        return(num)
+        
+                
