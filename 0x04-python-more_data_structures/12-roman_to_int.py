30d1181b39153f64e08c1e1e1f9ef41f425808a7
ascertain <kas.naya@gmail.com>
2023-06-14 13:59:22 -0700

    test for roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
+
 def roman_to_int(roman_string):
     if (not isinstance(roman_string, str) or
             roman_string is None):
-        return (0)
+        return 0
 
     roman = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
     }
     num = 0
     upper = roman_string.upper()
     for i in range(len(upper)):
         if roman.get(upper[i], 0) == 0:
-            return (0)
+            return 0
 
-        if (i != (len(upper) - 1) and roman[upper[i]] < roman[upper[i + 1]]):
+        if i != (len(upper) - 1) and roman[upper[i]] < roman[upper[i + 1]]:
             num += roman[upper[i]] * -1
-
         else:
             num += roman[upper[i]]
-    return (num)
+    return num
