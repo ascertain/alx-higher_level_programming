2bb50df0c9ceb0b629d1cd54f309eaf529a4ea56
ascertain <kas.naya@gmail.com>
2023-06-14 14:19:23 -0700

    test for roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 
-def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) is not str:
-        return 0
-    int_sum = 0
-    converter = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
-    for current, next in zip(roman_string, roman_string[1:]):
-        if converter[current] >= converter[next]:
-            int_sum += converter[current]
-        else:
-            int_sum -= converter[current]
-    int_sum += converter[roman_string[-1]]
-    return int_sum
+class Solution(object):
+    def romanToInt(self, s):
+        roman = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        total = 0
+        for i in range(len(s) - 1):
+            if roman[s[i]] < roman[s[i + 1]]:
+                total -= roman[s[i]]
+            else:
+                total += roman[s[i]]
+        return total + roman[s[-1]]
