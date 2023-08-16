774311181f12408398af9488e93897345bf94d15
ascertain <kas.naya@gmail.com>
2023-08-16 10:22:15 -0700

    1-create user update

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,9 @@
--- 0x0E. SQL - More queries, task 1. Root user
--- Creates user `user_0d_1`, sets pwd, and grants all privileges.
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES 
-ON *.*
-TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant USAGE and SELECT privileges to the user on the specified database
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
