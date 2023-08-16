d179502e14a57faeba89625fbdb4bb42c74b26f3
ascertain <kas.naya@gmail.com>
2023-08-16 05:57:09 -0700

    0-privilage update

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,17 +1,6 @@
-# Create user user_0d_1
-echo "CREATE USER 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
-
-# Grant privileges to user_0d_1
-echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
-
-# Create user user_0d_2
-echo "CREATE USER 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
-
-# Grant privileges to user_0d_2
-echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
 
 # List privileges for user_0d_1
-echo "SHOW GRANTS FOR 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
 # List privileges for user_0d_2
-echo "SHOW GRANTS FOR 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
